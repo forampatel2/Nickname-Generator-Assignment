82ece051243003b1c263887d8a76147eea8d6dfe
forampatel2 <78555940+forampatel2@users.noreply.github.com>
2021-02-18 10:30:13 -0700

    Add files via upload

---
+++ nickname generator assignment/main.js
@@ -0,0 +1,25 @@
+// Nickname Generator
+
+let nickname = ["'Crusher'", "'the Scientist", "'Twinkle-toes'", "'the Coder'", "'the Jester'", "'the Sloth'", "'Quick-Silver'"];
+
+// Variables for HTML Elements
+
+let resultsDivEl = document.getElementById("results");
+let firstNameEl = document.getElementById("firstName");
+let lastNameEl = document.getElementById("lastName");
+let randBtnEL = document.getElementById("random-btn");
+let allBtnEL = document.getElementById("all-btn");
+
+// Event Listeners
+
+randBtnEL.addEventListener("click", generateRandBtn);
+allBtnEL.addEventListener("click" , allNamesOut);
+
+function generateRandBtn(){
+    // Get first and last name
+    let firstName = firstName.value;
+    let lastName = lastName.value;
+
+    
+}
+
---
+++ nickname generator assignment/random-library.js
@@ -0,0 +1,11 @@
+// Return a random decimal between low (inclusive) and high (exclusive)
+function randomDec(low, high) {
+    return Math.random() * (high - low) + low;
+}
+
+// Return a random integer between low (inclusive) and high (exclusive)
+function randomInt(low, high) {
+    return Math.floor(Math.random(low, high));
+
+}
+
